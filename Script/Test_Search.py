f9d40713fddd669f8a0cc8d132b1f73f8a55efb4
Shirdy <17600943219@163.com>
2021-08-31 14:59:51 +0800

    添加测试用例

--- Script/Test_Search.py
+++ Script/Test_Search.py
@@ -15,24 +15,33 @@ def read_yaml():
 
 class Test_Search(object):
     def setup_class(self):
-        self.driver = init_driver()
-        self.baidu_search = Page(self.driver).re_baidu_search()
+        # self.driver = init_driver()
+        # self.baidu_search = Page(self.driver).re_baidu_search()
+        pass
 
     def teardown_class(self):
-        self.driver.quit()
+        # self.driver.quit()
+        pass
 
-    @pytest.fixture(scope='class')
-    def atest_001(self):
-        self.baidu_search.click_search()
+    # @pytest.fixture(scope='class')
+    # def atest_001(self):
+    #     self.baidu_search.click_search()
+    #
+    # @pytest.mark.usefixtures('atest_001')
+    # @pytest.mark.parametrize("case_num, text", read_yaml())
+    # @allure.step(title="百度搜索")
+    # @allure.severity("critical")
+    # def test_002(self, case_num, text):
+    #     allure.attach('测试编号', case_num)
+    #     self.baidu_search.input_search(text)
+    #     self.baidu_search.search()
 
-    @pytest.mark.usefixtures('atest_001')
-    @pytest.mark.parametrize("case_num, text", read_yaml())
-    @allure.step(title="百度搜索")
+    @pytest.mark.parametrize("text", [1, 2, 3])
+    @allure.step(title="比较")
     @allure.severity("critical")
-    def test_002(self, case_num, text):
-        allure.attach('测试编号', case_num)
-        self.baidu_search.input_search(text)
-        self.baidu_search.search()
+    def test_001(self, text):
+        allure.attach('测试编号', text)
+        assert text == 2
 
 
 if __name__ == '__main__':
